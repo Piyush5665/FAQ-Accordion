bbc8da20d07045afb146356937ab78afcc020318
Piyush Sharma <piyush14sharma2004@gmail.com>
2023-04-14 14:37:56 +0530

    ebob

---
+++ script.js
@@ -0,0 +1,21 @@
+const acc = document.querySelectorAll(".accordion");
+
+
+
+acc.forEach((element) => {
+  const open_icon = element.querySelector(".plus_icon");
+  const close_icon = element.querySelector(".cross_icon");
+
+  element.addEventListener("click", () => {
+    if (element.classList.contains("active")) {
+      element.classList.remove("active");
+      // open_icon.style.display = "block";
+      // close_icon.style.display = "none";
+    }
+     else {
+      element.classList.add("active");
+      // open_icon.style.display = "none";
+      // close_icon.style.display = "block";
+    }
+  });
+});
